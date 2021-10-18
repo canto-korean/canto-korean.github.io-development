21ec5d55a2978f3ec8899baf47cac4c0f5a05ee0
winghimjns <winghim_01@hotmail.com>
2021-10-19 01:04:33 +0800

    WIP feature : fetch data from spreadsheet, next up is the processing and optimizing the speed as the data has like half of a megabyte

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.scss';
+import {useEffect, useState} from 'react';
+import Loading from "./components/Loading";
+import Result from "./components/Result";
+import csvtojson from 'csvtojson';
+
 
 function App() {
+  const [spreadsheetData, setSpreadsheetData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://docs.google.com/spreadsheets/d/106i6RLyxQYh-jgEnPxZ3TX3C-VT3-k7vY-7gdfoLTyI/export?format=csv')
+      .then(response => response.text())
+      .then(csv => csvtojson({noheader: true, output: 'csv'}).fromString(csv))
+      .then(arr => arr.slice(1))
+      .then(data => data.map(row => row.filter(Boolean)))
+      .then(setSpreadsheetData);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+        {Array.isArray(spreadsheetData) ? <Result spreadsheetData={spreadsheetData} /> : <Loading />}
     </div>
   );
 }
